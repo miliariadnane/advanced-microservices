6f2f1c8467c1bbad177cefd3eb895a133ae16de8
miliariadnane <miliari.adnane@gmail.com>
2023-02-12 22:29:09 +0100

    add apiKey manager to gateway using client interface

--- gateway/src/main/java/dev/nano/gateway/security/ApiKeyAuthorizationCheckerImpl.java
+++ gateway/src/main/java/dev/nano/gateway/security/ApiKeyAuthorizationCheckerImpl.java
@@ -1,11 +1,19 @@
 package dev.nano.gateway.security;
 
+import dev.nano.clients.apiKeyManager.ApplicationName;
+import dev.nano.clients.apiKeyManager.apiKey.ApiKeyManagerClient;
+import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Service;
 
 @Service("main-checker")
+@AllArgsConstructor
 public class ApiKeyAuthorizationCheckerImpl implements ApiKeyAuthorizationChecker {
+    private final ApiKeyManagerClient apiKeyManagerClient;
     @Override
     public boolean isAuthorized(String apiKey, String applicationName) {
-        return true;
+        return apiKeyManagerClient.isKeyAuthorizedForApplication(
+                apiKey,
+                applicationName
+        ).isAuthorized();
     }
 }
