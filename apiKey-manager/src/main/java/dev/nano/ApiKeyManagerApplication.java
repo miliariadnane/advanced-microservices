cd3bda6de7d515fd2120f3ebf885c2bd62c2d557
MILIARI Adnane <52249758+miliariadnane@users.noreply.github.com>
2023-02-16 23:44:55 +0100

    db seeder using command line runner for Api key Manager

--- apiKey-manager/src/main/java/dev/nano/ApiKeyManagerApplication.java
+++ apiKey-manager/src/main/java/dev/nano/ApiKeyManagerApplication.java
@@ -1,5 +1,8 @@
 package dev.nano;
 
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
 public class ApiKeyManagerApplication {
     public static void main(String[] args) {
         System.out.println("Hello world!");
